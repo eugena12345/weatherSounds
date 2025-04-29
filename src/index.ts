e7d83aca434f0844a5fe85ee7df65a7c1c7e5175
Eugena <eugena.h12345@gmail.com>
2025-04-29 21:44:59 +0300

    add html and styles and dev server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import './style.scss';
+ 
 const greeting: string = "Hello, TypeScript with Webpack!";
 console.log(greeting);
+
+const rain = document.getElementsByClassName('rain');
+console.log(rain);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
-  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'), 
+    port: 3000, 
+    open: true, 
+    hot: true,  
+    compress: true,
+  },
+
+  mode: "development",
 };
